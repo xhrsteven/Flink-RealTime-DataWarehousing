379bc381b1a002ab6cbf5f7498e4b3d7b3335957
steven.xu <steven.xu@vantopgroup.com>
2021-03-15 23:37:55 +0800

    封装查询Phoenix工具类

---
+++ src/main/java/com/atguigu/gmall/realtime/utils/PhoenixUtil.java
@@ -0,0 +1,91 @@
+package com.atguigu.gmall.realtime.utils;
+
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.gmall.realtime.common.GmallConfig;
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 从Phoenix中查询数据
+ */
+public class PhoenixUtil {
+    private static Connection conn = null;
+
+    public static void init(){
+        try {
+            //注册驱动
+            Class.forName("org.apache.phoenix.jdbc.PhoenixDriver");
+            //获取链接
+            conn = DriverManager.getConnection(GmallConfig.PHOENIX_SERVER);
+            //指定操作的表空间
+            conn.setSchema(GmallConfig.HBASE_SCHEMA);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //从Phoenix中查询
+    public static <T> List<T> queryList(String sql, Class<T> clazz){
+        if(conn == null){
+            init();
+        }
+        List<T> resultList = new ArrayList<>();
+        PreparedStatement ps = null;
+        ResultSet rs =null;
+
+        try {
+            //获取数据库操作对象
+            ps = conn.prepareStatement(sql);
+            //执行sql
+            rs = ps.executeQuery();
+            //通过结果集获取元数据信息
+            ResultSetMetaData metaData = rs.getMetaData();
+
+            //处理结果集
+            while (rs.next()){
+                //声明一个对象，用于封装查询的一条结果集
+                T rowData = clazz.newInstance();
+                for (int i = 1; i <= metaData.getColumnCount(); i++){
+                    BeanUtils.setProperty(rowData,metaData.getColumnName(i),rs.getObject(i));
+                }
+                resultList.add(rowData);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("从维度表查询失败");
+        }finally{
+            //释放资源
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+        return resultList;
+    }
+
+    public static void main(String[] args) {
+        queryList("select * from DIM_BASE_TRADEMARK", JSONObject.class);
+    }
+
+}
